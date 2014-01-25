ddb1d7218b2764fe74ba7cbe4dbe1798feaac40b
AzotKZN <Miatozoff@gmail.com>
2014-01-25 14:53:13 +0400

    First

---
+++ src/com/example/monkeytest4/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.monkeytest4;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	 
+	 TextView tvOut;
+	 Button btnOk;
+	
+	 
+	 /** Called when the activity is first created. */
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	 super.onCreate(savedInstanceState);
+	 setContentView(R.layout.activity_main);
+	 
+	 // найдем View-элементы
+	 tvOut = (TextView) findViewById(R.id.tvOut);
+	 btnOk = (Button) findViewById(R.id.btnOk); 
+	 
+	 // создаем обработчик нажатия
+	 OnClickListener oclBtnOk = new OnClickListener() {
+	 @Override
+	 public void onClick(View v) {
+	 // Меняем текст в TextView (tvOut)
+		 Random r = new Random();
+		 int i1=r.nextInt(100);
+	 if (i1 > 50) {
+		 tvOut.setText("Hello!");
+	} else
+	{
+		tvOut.setText("");
+	}
+	 }
+	 };
+	 
+	 // присвоим обработчик кнопке OK (btnOk)
+	 btnOk.setOnClickListener(oclBtnOk);
+	 }
+	 }
+
